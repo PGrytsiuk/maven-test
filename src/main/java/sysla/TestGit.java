1170612e0dbf3120954234ed1c005df5864d6315
PGrytsiuk <33345335+PGrytsiuk@users.noreply.github.com>
2019-10-31 18:14:18 +0200

    Update TestGit.java

--- src/main/java/sysla/TestGit.java
+++ src/main/java/sysla/TestGit.java
@@ -1,5 +1,13 @@
 package sysla;
 
 public class TestGit {
+  
+  public void testbody() throws IOException {
+		  
+		  Response resp = RestAssured.get("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+		  
+		 String data =resp.asString();
+    
+  }
 
 }
